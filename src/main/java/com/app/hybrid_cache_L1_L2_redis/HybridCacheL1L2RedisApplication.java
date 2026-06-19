c313ba7c8477e95d57299845148aea0f950276ec
Erwin Corzo <erwincorzo@MacBook-Pro-de-Erwin.local>
2026-06-19 10:31:58 -0500

    first commit

---
+++ src/main/java/com/app/hybrid_cache_L1_L2_redis/HybridCacheL1L2RedisApplication.java
@@ -0,0 +1,13 @@
+package com.app.hybrid_cache_L1_L2_redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HybridCacheL1L2RedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HybridCacheL1L2RedisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/hybrid_cache_L1_L2_redis/HybridCacheL1L2RedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.hybrid_cache_L1_L2_redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HybridCacheL1L2RedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
